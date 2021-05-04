139f65261969ebb2221dce1a63ce469845e72605
Iñaki Cabo <icabo@rappi.com>
2021-05-04 16:38:38 -0500

    generate curp from curp form

--- mi_vacuna/lib/ui/views/curp/curp_view.dart
+++ mi_vacuna/lib/ui/views/curp/curp_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:mi_vacuna/ui/views/curp/curp_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
@@ -11,16 +12,17 @@ class CurpView extends StatelessWidget {
         toolbarHeight: 96,
         elevation: 0,
         centerTitle: true,
-        title: Image.network(
-          'https://lh3.googleusercontent.com/proxy/RzG4z7ir2VxoWfKxfKwA02Um_tdhKr9xsRAL9S1pOtaFv-T4koIdhqpczcaRq46t5riE_fRf8-2BsNT0cUYKTpXHsM8fxtcityNBzJZTE2NfLZaNGrw',
+        title: Image.asset(
+          'assets/images/gob.png',
           height: 72,
         ),
       ),
       body: Center(
         child: ViewModelBuilder<CurpViewModel>.reactive(
           builder: (context, model, child) => Form(
+            key: model.formKey,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 72),
+              padding: const EdgeInsets.symmetric(horizontal: 256),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
@@ -29,32 +31,50 @@ class CurpView extends StatelessWidget {
                         Expanded(
                           child: TextFormField(
                             decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xFF9D0A0A),
                                 ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Nombre(s)'),
+                              ),
+                              labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
+                              border: OutlineInputBorder(),
+                              labelText: 'Nombre(s)',
+                            ),
+                            onSaved: (value) {
+                              model.user['first_name'] = value;
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Debes ingresar un tu nombre';
+                              }
+                            },
                           ),
                         ),
                         SizedBox(width: 32),
                         Expanded(
                           child: TextFormField(
                             decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xFF9D0A0A),
                                 ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Primer Apellido'),
+                              ),
+                              labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
+                              border: OutlineInputBorder(),
+                              labelText: 'Primer Apellido',
+                            ),
+                            onSaved: (value) {
+                              model.user['last_name'] = value;
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Debes ingresar un tu primer apellido';
+                              }
+                            },
                           ),
                         ),
                       ],
@@ -65,32 +85,79 @@ class CurpView extends StatelessWidget {
                         Expanded(
                           child: TextFormField(
                             decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xFF9D0A0A),
                                 ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Segundo Apellido'),
+                              ),
+                              labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
+                              border: OutlineInputBorder(),
+                              labelText: 'Segundo Apellido',
+                            ),
+                            onSaved: (value) {
+                              model.user['surname'] = value;
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Debes ingresar un tu segundo apellido';
+                              }
+                            },
                           ),
                         ),
                         SizedBox(width: 32),
                         Expanded(
                           child: TextFormField(
                             decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xFF9D0A0A),
                                 ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Día de nacimiento'),
+                              ),
+                              labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
+                              border: OutlineInputBorder(),
+                              labelText: 'Fecha de nacimiento',
+                            ),
+                            readOnly: true,
+                            onTap: () async {
+                              final result = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime.now()
+                                      .subtract(Duration(days: 100 * 360)),
+                                  lastDate: DateTime.now(),
+                                  builder: (context, child) {
+                                    return Theme(
+                                      data: Theme.of(context).copyWith(
+                                        primaryColor: Color(0xFF9d0A0A),
+                                        accentColor: const Color(0xFF9d0A0A),
+                                        colorScheme: ColorScheme.light(
+                                          primary: const Color(0xFF9d0A0A),
+                                        ),
+                                      ),
+                                      child: child!,
+                                    );
+                                  });
+
+                              if (result != null) {
+                                model.user['birth_date'] = result;
+                                model.notifyListeners();
+                              }
+                            },
+                            controller: TextEditingController(
+                              text: model.user['birth_date'] != null
+                                  ? DateFormat('dd/MM/yyyy')
+                                      .format(model.user['birth_date'])
+                                  : '',
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Debes ingresar un una fecha de nacimiento';
+                              }
+                            },
                           ),
                         ),
                       ],
@@ -99,70 +166,103 @@ class CurpView extends StatelessWidget {
                     Row(
                       children: [
                         Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
-                                ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Mes de nacimiento'),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.white,
+                            ),
+                            child: DropdownButtonFormField<String>(
+                              value: model.user['gender'],
+                              isExpanded: true,
+                              items: ['Hombre', 'Mujer']
+                                  .map((value) => DropdownMenuItem(
+                                        child: Text(value),
+                                        value: value,
+                                      ))
+                                  .toList(),
+                              hint: Text(
+                                'Sexo',
+                                style: TextStyle(color: Color(0xFF9D0A0A)),
+                              ),
+                              onChanged: (value) {
+                                model.user['gender'] = value;
+                                model.notifyListeners();
+                              },
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Debes ingresar un tu sexo';
+                                }
+                              },
+                            ),
                           ),
                         ),
                         SizedBox(width: 32),
                         Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
-                                ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Año de nacimiento'),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
-                                ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Sexo'),
-                          ),
-                        ),
-                        SizedBox(width: 32),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0xFF9D0A0A),
-                                  ),
-                                ),
-                                labelStyle: TextStyle(color: Color(0xFF9D0A0A)),
-                                border: OutlineInputBorder(),
-                                labelText: 'Estado de nacimiento'),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.white,
+                            ),
+                            child: DropdownButtonFormField<String>(
+                              value: model.user['state'],
+                              isExpanded: true,
+                              items: [
+                                'Aguascalientes',
+                                'Baja California',
+                                'Baja California Sur',
+                                'Campeche',
+                                'Coahuila',
+                                'Colima',
+                                'Chiapas',
+                                'Chihuahua',
+                                'Ciudad de México',
+                                'Durango',
+                                'Guanajuato',
+                                'Guerrero',
+                                'Hidalgo',
+                                'Jalisco',
+                                'México',
+                                'Michoacán',
+                                'Morelos',
+                                'Nayarit',
+                                'Nuevo León',
+                                'Oaxaca',
+                                'Puebla',
+                                'Querétaro',
+                                'Quintana Roo',
+                                'San Luis Potosí',
+                                'Sinaloa',
+                                'Sonora',
+                                'Tabasco',
+                                'Tamaulipas',
+                                'Tlaxcala',
+                                'Veracruz',
+                                'Yucatán',
+                                'Zacatecas'
+                              ]
+                                  .map((value) => DropdownMenuItem(
+                                        child: Text(value),
+                                        value: value,
+                                      ))
+                                  .toList(),
+                              hint: Text(
+                                'Estado de nacimiento',
+                                style: TextStyle(color: Color(0xFF9D0A0A)),
+                              ),
+                              onChanged: (value) {
+                                model.user['state'] = value;
+                                model.notifyListeners();
+                              },
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Debes ingresar un tu estado de nacimiento';
+                                }
+                              },
+                            ),
                           ),
                         ),
                       ],
@@ -172,7 +272,7 @@ class CurpView extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         ElevatedButton(
-                          onPressed: model.showResult,
+                          onPressed: model.search,
                           child: Text(
                             'Consultar',
                             style: TextStyle(
--- mi_vacuna/lib/ui/views/curp/curp_viewmodel.dart
+++ mi_vacuna/lib/ui/views/curp/curp_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'package:mi_vacuna/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -6,15 +8,28 @@ class CurpViewModel extends BaseViewModel {
   final _dialogService = locator<DialogService>();
   final _navigationService = locator<NavigationService>();
 
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  final user = {};
+
   showResult() {
-    final curp = 'XXXX010101XXAA';
+    final curp =
+        '${user['last_name'].toString().substring(0, 1).toUpperCase()}${user['surname'].toString()[0].toUpperCase()}${user['first_name'].toString()[0]}${DateFormat('yyMMdd').format(user['birth_date'])}XAXAXA00';
 
     _dialogService
         .showDialog(
           title: 'Resultado',
-          description: 'Tu CURP es:\n${curp}',
+          description: 'Tu CURP es:\n$curp',
           dialogPlatform: DialogPlatform.Material,
         )
         .whenComplete(() => _navigationService.back(result: curp));
+  }
+
+  search() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState?.save();
+
+      showResult();
+    }
   }
 }

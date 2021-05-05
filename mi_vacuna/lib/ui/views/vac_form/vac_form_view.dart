1e05fea03ad1b2ab56a6272a71795f92b6534edf
asanc <sanchezorosco_alexis@hotmail.com>
2021-05-04 19:38:09 -0500

    imagen de fondo

--- mi_vacuna/lib/ui/views/vac_form/vac_form_view.dart
+++ mi_vacuna/lib/ui/views/vac_form/vac_form_view.dart
@@ -9,8 +9,8 @@ class VacFormView extends StatelessWidget {
         toolbarHeight: 96,
         elevation: 0,
         centerTitle: true,
-        title: Image.network(
-          'https://lh3.googleusercontent.com/proxy/RzG4z7ir2VxoWfKxfKwA02Um_tdhKr9xsRAL9S1pOtaFv-T4koIdhqpczcaRq46t5riE_fRf8-2BsNT0cUYKTpXHsM8fxtcityNBzJZTE2NfLZaNGrw',
+        title: Image.asset(
+          'assets/images/gob.png',
           height: 72,
         ),
         //title: Image.asset('assets/images/logo.png'),
